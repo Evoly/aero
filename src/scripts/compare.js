df500f215369691fa235a2736b93644e2a2ef8d2
Sveta <svetoch.sv@gmail.com>
2019-07-17 17:06:50 +0700

    comparision fixed

--- src/scripts/compare.js
+++ src/scripts/compare.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   //определяем высоту ячеек
-  const RowHeight = (row) => {
+  var RowHeight = (row) => {
     let maxHeight = 0;
 
     $('.comparision [data-row="' + row + '"]').each(function() {
@@ -18,20 +18,20 @@ $(document).ready(function() {
     $('.comparision__names').addClass('visible');
   };
 
-  const SetHeight = () => {
+  var SetHeight = () => {
     $('.comparision__name').each(function() {
       let el = $(this).data('row');
       RowHeight(el);
     });
   };
 
-  const owlOptions_compareSlider = {
+  var owlOptions_compareSlider = {
     margin: 0,
     loop: false,
     autoWidth: true,
     items: 4,
     nav: true,
-    navClass: [ 'owl-prev nav-fixed', 'owl-next nav-fixed' ],
+    navClass: ['owl-prev nav-fixed', 'owl-next nav-fixed'],
     navText: ['<span class="icon arrow-next-sm"></span><span class="icon arrow-next-sm_red"></span>',
       '<span class="icon arrow-next-sm"></span><span class="icon arrow-next-sm_red"></span>'
     ],
@@ -47,7 +47,7 @@ $(document).ready(function() {
   //row:hover
 
   $('.comparision [data-row]').mouseenter(function() {
-    const rowNumber = $(this).data('row');
+    var rowNumber = $(this).data('row');
     $('.comparision [data-row]').filter('[data-row="' + rowNumber + '"]').addClass('hover');
   });
 
@@ -58,8 +58,8 @@ $(document).ready(function() {
   //product card:hover
 
   $('.comparision .product-card').mouseenter(function() {
-    const currentCard = $(this);
-    const nextCard = currentCard.closest('.owl-item').next()
+    var currentCard = $(this);
+    var nextCard = currentCard.closest('.owl-item').next()
     nextCard.find('.product-card').addClass('hover');;
   });
 
@@ -67,26 +67,42 @@ $(document).ready(function() {
     $('.comparision .product-card').removeClass('hover');
   });
 
+
+  $(window).scroll(function() {
+    $('.js-sticky').each(function() {
+      var scrolled = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+      var top = $(this).offset().top;
+      var bottom = $('.comparision').offset().top + $('.comparision').outerHeight();
+      var translate = (scrolled) < (bottom - 300) ? (scrolled - top) : (bottom - top - 300);
+      var border = $('.product-card__sticky-wrapper').offset().top
+
+      if (scrolled > top) {
+        $(this).children().css('transform', 'translateY(' + translate + 'px)');
+        $(this).children().addClass("sticky");
+        if (scrolled > border) {
+          $('.sticky').addClass('sticky_shadow')
+        } else {
+          $('.sticky').removeClass('sticky_shadow')
+        }
+      } else {
+        $(this).children().css('transform', '');
+        $('.sticky').removeClass('sticky_shadow')
+        $(this).children().removeClass('sticky');
+      }
+    });
+  });
+
   if ($('.nav-fixed').length > 0) {
     $('.nav-fixed').each(function() {
-
-      const top = $(this).offset().top;
-      const bottom = $(this).offset().top + $(this).outerHeight();
-      const iconPOsition = 280;
-
+      var top = $(this).offset().top;
+      var bottom = $(this).offset().top + $(this).outerHeight();
+      var iconPOsition = 280; // css
       window.onscroll = function() {
-
-        var scrolled = window.scrollY;
-
+        var scrolled = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
         var elHeight = $('.nav-fixed').outerHeight();
-
         if (scrolled > top) {
-          const translate = (scrolled ) < (bottom - iconPOsition - 60) ? (scrolled - top) : (bottom - iconPOsition - top-100);
-          console.log('scrolled', scrolled);
-          console.log('bottom', bottom - iconPOsition - 100);
-
+          var translate = (scrolled) < (bottom - iconPOsition - 60) ? (scrolled - top) : (bottom - iconPOsition - top - 100);
           $('.nav-fixed').css('transform', 'translateY(' + translate + 'px)');
-
         } else {
           $('.nav-fixed').css('transform', '');
 
@@ -95,4 +111,15 @@ $(document).ready(function() {
     });
   }
 
+  if ($(window).width() > 768) {
+    $('.comparision__header-wrapper').children('.comparision__sticky').addClass('js-sticky');
+  }
+
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $('.comparision__header-wrapper').children('.comparision__sticky').removeClass('js-sticky');
+    } else {
+      $('.comparision__header-wrapper').children('.comparision__sticky').addClass('js-sticky');
+    }
+  });
 });
